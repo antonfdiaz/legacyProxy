033048f0031d70467cee78f7f86d7487db13caae
t0nchi7 <159287835+antonfdiaz@users.noreply.github.com>
2026-07-16 11:03:12 +0200

    remove fonts

--- main.py
+++ main.py
@@ -20,36 +20,12 @@ class InterceptAddon:
         print(f"[INFO] intercepted request to: {flow.request.url}")
         host = flow.request.pretty_host
         url = flow.request.url
-        
-        if host in GOOGLE_HOSTS and flow.request.path == "/fonts/googlesans.ttf":
-            flow.response = http.Response.make(
-                200,
-                (Path(__file__).parent/"fonts"/"googlesans.ttf").read_bytes(),
-                {"Content-Type": "font/ttf; charset=utf-8","Cache-Control": "public,max-age=86400"},
-            )
-            return
-        
-        if host in GOOGLE_HOSTS and flow.request.path == "/fonts/roboto.ttf":
-            flow.response = http.Response.make(
-                200,
-                (Path(__file__).parent/"fonts"/"roboto.ttf").read_bytes(),
-                {"Content-Type": "font/ttf; charset=utf-8","Cache-Control": "public,max-age=86400"},
-            )
-            return
 
         if host in GOOGLE_HOSTS and flow.request.path == "/images/google.png":
             flow.response = http.Response.make(
                 200,
                 (Path(__file__).parent/"images"/"google.png").read_bytes(),
                 {"Content-Type": "image/png","Cache-Control": "public,max-age=86400"},
-            )
-            return
-        
-        if host in GOOGLE_HOSTS and flow.request.path == "/images/loader.gif":
-            flow.response = http.Response.make(
-                200,
-                (Path(__file__).parent/"images"/"loader.gif").read_bytes(),
-                {"Content-Type": "image/gif","Cache-Control": "public,max-age=86400"},
             )
             return
         
