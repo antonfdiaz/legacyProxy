dec4578f01788cf655fd795d65d2257b89c3c904
t0nchi7 <159287835+antonfdiaz@users.noreply.github.com>
2026-08-16 13:50:57 +0200

    remove build script

--- build_mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-nuitka --standalone --macos-create-app-bundle --macos-app-icon=./images/lpbigsur.png --include-data-dir=./js=js --include-data-dir=./html=html --include-data-dir=./css=css --include-data-dir=./images=images main.py
